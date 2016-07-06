96062217f180bb813e06f336c22476e9dcfdd208
LeeJHkorean <dlwogur1126@naver.com>
2016-07-06 15:56:51 +0900

    commit request

--- fork_full_request.c
+++ fork_full_request.c
@@ -3,9 +3,10 @@
 int main()
 {
   int i;
-  for (i = 0; i < 5; i++) {
+  printf*("구구단 출력\n");
+  for (i = 0; i < 9; i++) {
     for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+      printf("%dx%d=%d \n", i, j, i * j);
     }
   }
   
